404435e4f68ec07a5c40f8a032c3d07049598624
Roberto Fernandes <roberto.fernandes21@gmail.com>
2018-11-07 14:10:15 +0000

    Model refactoring

---
+++ app/src/main/java/robfernandes/xyz/moodtracker/Model/Day.java
@@ -0,0 +1,31 @@
+package robfernandes.xyz.moodtracker.Model;
+
+/**
+ * Created by Roberto Fernandes on 07/11/2018.
+ */
+public class Day {
+    private MoodType moodType;
+    private String note;
+
+    public Day(MoodType moodType) {
+        this.moodType = moodType;
+        setNote("");
+    }
+
+    public Day(MoodType moodType, String note) {
+        this(moodType);
+        this.note = note;
+    }
+
+    public MoodType getMoodType() {
+        return moodType;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+}
