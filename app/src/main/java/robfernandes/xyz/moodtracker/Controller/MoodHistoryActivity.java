106fc7515760d9d8b4bd5f78bc09964df7a0ba37
Roberto Fernandes <roberto.fernandes21@gmail.com>
2018-11-07 10:41:35 +0000

    Mood History xml

---
+++ app/src/main/java/robfernandes/xyz/moodtracker/Controller/MoodHistoryActivity.java
@@ -0,0 +1,15 @@
+package robfernandes.xyz.moodtracker.Controller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import robfernandes.xyz.moodtracker.R;
+
+public class MoodHistoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mood_history);
+    }
+}
